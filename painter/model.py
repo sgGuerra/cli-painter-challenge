f878c084525b0d847392edbabdc17668a4595bcf
Luis Guerra <guerraherrera000@gmail.com>
2024-08-11 10:02:22 -0500

    corrección de la clase triangulo

--- painter/model.py
+++ painter/model.py
@@ -1,8 +1,10 @@
 import math
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
 
 class Point:
-    def __init__(self, x: float, y:float ):
+
+    def __init__(self, x: float, y: float):
         self.x: float = x
         self.y: float = y
 
@@ -12,10 +14,8 @@ class Circle:
         self.center: Point = center
         self.radius: float = radius
 
-
-    def area(self):
-        return float(math.pi*(self.radius**2))
-
+    def area(self) -> float:
+        return math.pi * (self.radius ** 2)
 
     def draw(self):
         circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
@@ -23,20 +23,30 @@ class Circle:
         plt.axis("scaled")
         plt.show()
 
-
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Circle with center at ({self.center.x}, {self.center.y}) and radius {self.radius}"
 
 
 class Triangle:
-    def __init__(self,point_1: Point, point_2: Point, point_3: Point ):
-        self.point_1 = point_1
-        self.point_2 = point_2
-        self.point_3 = point_3
 
+    def __init__(self, point_1: Point, point_2: Point, point_3: Point):
+        self.point_1: Point = point_1
+        self.point_2: Point = point_2
+        self.point_3: Point = point_3
 
-    def area(self):
+    def area(self) -> float:
+        return 0.5 * abs((self.point_1.x * self.point_2.y - self.point_1.x * self.point_3.y) +
+                         (self.point_2.x * self.point_3.y - self.point_2.x * self.point_1.y) +
+                         (self.point_3.x * self.point_1.y - self.point_3.x * self.point_2.y))
 
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x]
+        y = [self.point_1.y, self.point_2.y, self.point_3.y, self.point_1.y]
+        plt.fill(x, y, color='b')
+        plt.axis("scaled")
+        plt.show()
 
-
+    def __str__(self) -> str:
+        return f"Triangle with vertices at ({self.point_1.x}, {self.point_1.y}), ({self.point_2.x}, {self.point_2.y}) \
+        and ({self.point_3.x}, {self.point_3.y})"
 
